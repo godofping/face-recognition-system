7100fd4d3d150e9131a88ff02265d81dee014f78
Rex Louis <rexmicrosoft@yahoo.com.ph>
2021-10-14 14:27:24 +0800

    source code

---
+++ Face Recognition Attendance System/Face Recognition Attendance System/Classes/Admin.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Admin
+    Dim adminid As Integer
+    Dim username As String
+    Dim password As String
+
+    Public Property _Adminid As Integer
+        Get
+            Return adminid
+        End Get
+        Set(value As Integer)
+            adminid = value
+        End Set
+    End Property
+
+    Public Property _Username As String
+        Get
+            Return username
+        End Get
+        Set(value As String)
+            username = value
+        End Set
+    End Property
+
+    Public Property _Password As String
+        Get
+            Return password
+        End Get
+        Set(value As String)
+            password = value
+        End Set
+    End Property
+
+    Public Function Login(ByVal admin As Admin) As DataTable
+        Using cmd = New SqlCommand()
+            cmd.CommandText = "select * from admin where username = @username and password = @password"
+            cmd.Parameters.AddWithValue("@username", admin._Username)
+            cmd.Parameters.AddWithValue("@password", admin._Password)
+            Return Helper.executeQuery(cmd)
+        End Using
+    End Function
+End Class
---
+++ Face Recognition Attendance System/Face Recognition Attendance System/Forms/LoginForm.vb
@@ -0,0 +1,18 @@
+﻿Public Class LoginForm
+    Dim admin As New Admin
+
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        admin._Username = txtUsername.Text
+        admin._Password = txtPassword.Text
+
+        Dim dt As DataTable = admin.Login(admin)
+
+        If dt.Rows.Count > 0 Then
+            MsgBox("Welcome " & dt.Rows(0)("username"))
+        Else
+            MsgBox("Login Failed")
+        End If
+
+    End Sub
+End Class
